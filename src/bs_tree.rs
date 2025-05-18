9ebee58ce93036d2f7070a9144e4367e03d6b44b
Adamniels <adamnielsen401@gmail.com>
2025-05-18 20:37:16 +0200

    started on bst

--- src/bs_tree.rs
+++ src/bs_tree.rs
@@ -1,4 +1,56 @@
-pub struct BinarySearchTree {}
+pub struct Node {
+    nr: i32,
+    value: String,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
+}
+
+impl Node {
+    pub fn new(nr: i32, value: String) -> Self {
+        Node {
+            nr,
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn get_value(&self) -> &str {
+        &self.value
+    }
+
+    pub fn destroy(self) -> String {
+        self.value
+    }
+
+    pub fn set_left_node(&mut self, node: Node) {
+        self.left = Some(Box::new(node));
+    }
+
+    pub fn set_right_node(&mut self, node: Node) {
+        self.right = Some(Box::new(node));
+    }
+
+    pub fn get_left(&self) -> Option<&Box<Node>> {
+        self.left.as_ref()
+    }
+
+    pub fn get_right(&self) -> Option<&Box<Node>> {
+        self.right.as_ref()
+    }
+}
+
+// kan egentligen använda #[derive(Clone)]
+impl Clone for Node {
+    fn clone(&self) -> Self {
+        Node {
+            nr: self.nr.clone(),
+            value: self.value.clone(),
+            left: self.left.clone(),
+            right: self.right.clone(),
+        }
+    }
+}
 
 // Funktioner:
 //  - [ ] is_empty
@@ -10,4 +62,24 @@ pub struct BinarySearchTree {}
 //  - [ ] height
 //  - [ ] map - gör en funktion på alla element i trädet
 
-impl BinarySearchTree {}
+pub struct BinarySearchTree {
+    root: Option<Node>,
+}
+
+impl BinarySearchTree {
+    pub fn new() -> Self {
+        BinarySearchTree { root: None }
+    }
+
+    pub fn insert(&mut self, node: Node) {
+        todo!("insert")
+    }
+
+    pub fn is_empty(&self) -> bool {
+        todo!("is empty")
+    }
+
+    pub fn size(&self) -> usize {
+        todo!("size")
+    }
+}
--- src/tests/bs_tree_test.rs
+++ src/tests/bs_tree_test.rs
@@ -1,4 +1,61 @@
 use crate::bs_tree::BinarySearchTree;
+use crate::bs_tree::Node;
 
 #[test]
-fn simple_test() {}
+fn test_node_all_methods() {
+    // Skapa root-nod
+    let mut root = Node::new(10, String::from("root"));
+
+    // Kontrollera att get_value fungerar
+    assert_eq!(root.get_value(), "root");
+
+    // Skapa barnnoder
+    let left_child = Node::new(5, String::from("left"));
+    let right_child = Node::new(15, String::from("right"));
+
+    // Lägg till barnnoder
+    root.set_left_node(left_child);
+    root.set_right_node(right_child);
+
+    // Kontrollera att barnnoderna sattes korrekt
+    assert!(root.get_left().is_some());
+    assert!(root.get_right().is_some());
+
+    assert_eq!(root.get_left().as_ref().unwrap().get_value(), "left");
+    assert_eq!(root.get_right().as_ref().unwrap().get_value(), "right");
+
+    // Testa kloning
+    let clone = root.clone();
+    assert_eq!(clone.get_value(), "root");
+    assert_eq!(clone.get_left().as_ref().unwrap().get_value(), "left");
+    assert_eq!(clone.get_right().as_ref().unwrap().get_value(), "right");
+
+    // Testa destroy (tar över ägande)
+    let to_destroy = Node::new(99, String::from("destroy_me"));
+    let destroyed_value = to_destroy.destroy();
+    assert_eq!(destroyed_value, "destroy_me");
+    // (Man kan inte använda to_destroy här efteråt – ownership är borta!)
+}
+
+#[test]
+fn test_is_empty_insert_size() {
+    // Skapa ett tomt träd
+    let mut tree = BinarySearchTree::new();
+
+    // Det borde vara tomt direkt efter skapande
+    assert!(tree.is_empty(), "Tree should be empty at start");
+    assert_eq!(tree.size(), 0, "Tree size should be 0 at start");
+
+    // Lägg till en nod
+    tree.insert(Node::new(10, "root".to_string()));
+    assert!(!tree.is_empty(), "Tree should not be empty after insert");
+    assert_eq!(tree.size(), 1, "Tree size should be 1 after one insert");
+
+    // Lägg till fler noder
+    tree.insert(Node::new(5, "left".to_string()));
+    tree.insert(Node::new(15, "right".to_string()));
+    tree.insert(Node::new(3, "left-left".to_string()));
+    tree.insert(Node::new(7, "left-right".to_string()));
+
+    assert_eq!(tree.size(), 5, "Tree size should be 5 after five inserts");
+}
